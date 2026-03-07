d65be1794599176ca7b24306c73281f1c2b1dfa5
Ajaykapratwar <kapratwarajay@gmail.com>
2026-03-07 10:34:53 +0530

    feat: add logging for user validation API calls in UserService and UserValidationService

--- userservice/src/main/java/com/fitness/userservice/service/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -5,10 +5,12 @@ import com.fitness.userservice.dto.UserResponse;
 import com.fitness.userservice.model.User;
 import com.fitness.userservice.repository.UserRepository;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserService {
 
     private UserService userService;
@@ -58,6 +60,7 @@ public class UserService {
     }
 
     public Boolean existsByUserId(String userId) {
+        log.info("Calling user validation API for UserId: {}", userId);
         return userRepository.existsById(userId);
     }
 }
